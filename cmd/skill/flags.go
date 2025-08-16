e98269a0e48fd093ddb2dc38d1cdde7a310c4867
melifaro <melifaro.nbk@gmail.com>
2025-08-16 22:43:29 +0400

    Initial commit

---
+++ cmd/skill/flags.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"os"
+)
+
+// неэкспортированная переменная flagRunAddr содержит адрес и порт для запуска сервера
+var (
+	flagRunAddr  string
+	flagLogLevel string
+)
+
+// parseFlags обрабатывает аргументы командной строки
+// и сохраняет их значения в соответствующих переменных
+func parseFlags() {
+	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
+	flag.StringVar(&flagLogLevel, "l", "info", "log level")
+	flag.Parse()
+
+	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
+	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
+		flagLogLevel = envLogLevel
+	}
+}
